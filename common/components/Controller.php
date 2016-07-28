2ca054ad93f03853aafa0190819a77b361b06052
xiewulong <xiewulong@vip.qq.com>
2016-07-28 15:53:26 +0800

    app

--- common/components/Controller.php
+++ common/components/Controller.php
@@ -8,10 +8,22 @@ class Controller extends \yii\web\Controller {
 
 	public function init() {
 		parent::init();
+
+		if($referrer = \Yii::$app->request->get('referrer')) {
+			\Yii::$app->user->setReturnUrl($referrer);
+		}
 	}
 
-	public function respond($response = null, $format = \yii\web\Response::FORMAT_JSON) {
-		\Yii::$app->response->format = $format;
+	public function respond($response = null, $format = \yii\web\Response::FORMAT_JSON, $callbackParam = null) {
+		\Yii::$app->response->format = \Yii::$app->request->get('format', $format);
+		if(($callback = \Yii::$app->request->get($callbackParam)) && \Yii::$app->response->format == 'jsonp' && $response) {
+			$_response = $response;
+			$response = [
+				'data' => $_response,
+				'callback' => $callback,
+			];
+		}
+
 		return $response;
 	}
 
